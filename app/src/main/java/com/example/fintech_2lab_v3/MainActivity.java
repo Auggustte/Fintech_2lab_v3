f1395ab0c78e0d37e1e2575a9d7042d2471bc3a2
Auggustte <auguste95@gmail.com>
2022-03-16 20:35:25 +0200

    Secondary commit

--- app/src/main/java/com/example/fintech_2lab_v3/MainActivity.java
+++ app/src/main/java/com/example/fintech_2lab_v3/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.fintech_2lab_v3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    Spinner spOptionSelection;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvResult = findViewById(R.id.tvResult);
+        this.spOptionSelection = findViewById(R.id.spOptionSelection);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource( this,
+                R.array.counting_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        this.spOptionSelection.setAdapter(adapter);
+
+
+    }
+
+    public void onBtnCalculateClick(View view) {
+
+
     }
 }
